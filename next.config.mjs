9a0761327437ac59631d2c2d48a4ef1800beebf3
radarsmart <contato@radarsmart.com.br>
2026-03-26 13:25:18 -0300

    fix: remove bloco env incorreto do next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,8 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "**" },
-      { protocol: "http", hostname: "**" },
+      { protocol: "http", hostname: "***" },
     ],
-  },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
   },
 };
 
